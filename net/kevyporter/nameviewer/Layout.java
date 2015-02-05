01ee033501e2fbf4791b8b40a9c06aafd7b58d59
Kevy Porter <kevyporter@gmail.com>
2015-02-06 00:39:59 +1100

    v2.0

--- net/kevyporter/nameviewer/Layout.java
+++ net/kevyporter/nameviewer/Layout.java
@@ -1,47 +1,78 @@
 package net.kevyporter.nameviewer;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.TextArea;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+public class Layout {
 
-public class NameViewer {
+	protected JFrame createFrame(){
+		JFrame frame = new JFrame("Name Viewer");
 
-	public void ViewNames(JTextField field){
-		if(field.getText().equalsIgnoreCase("")){
-			Utils.SetText("Enter a name!");return;
-		}
-		HashMap<Integer, String> map = new HashMap<>();
-		String apiResponse = Utils.getAPI(field.getText());
-		if (apiResponse.equalsIgnoreCase("")) {
-			return;
-		}
-		if(apiResponse.replace('"', ' ').replace(" ", "").equalsIgnoreCase("{success:false,cause:Internal error}")){
-			Utils.SetText("Internal Error.\nTry again in a few moments!");
-		}
-		if (apiResponse.replace('"', ' ').replace(" ", "").equalsIgnoreCase("{cause:Keythrottle!,throttle:true,success:false}"))
-	    {
-	      Utils.SetText("Please try again in a few moments!\nThe API key got used to much!");return;
-	    }
-	    if (apiResponse.replace('"', ' ').replace(" ", "").contains("player:null"))
-	    {
-	      Utils.SetText("Player '" + field.getText() + "' not found!");return;
-	    }
-		getName(apiResponse, map);
+		int sizeX = 975; 
+		int sizeY = 880;
+
+		frame.setDefaultCloseOperation(3);
+		frame.setBounds(new Rectangle(sizeX, sizeY));
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setLayout(null);
+
+		frame.setContentPane(new JLabel(
+				new ImageIcon(
+						Toolkit.getDefaultToolkit().createImage(Layout.class.getResource("/net/kevyporter/nameviewer/background.png")))));
+		
+		return frame;
 	}
 
-	private void getName(String name, Map<Integer, String> map){
-		name = name.replace("\",\"", ".");
-		name = name.replace("{", "").replace("}", "").replace('"', ' ').replace(" ", "").replace("[", "").replace("]", "");
-		String[] split = name.split(Pattern.quote(","));
-		for(String s : split){
-			if(s.startsWith("knownAliases")){
-				map.put(Integer.valueOf(0), "Names: " + s.replace("knownAliases:", "").replace(".", ", ") + ".");
+	protected void viewName(JFrame frame){
+//		JLabel credit = new JLabel("By KevyPorter v1.0");
+//		credit.setBounds(105, 10, 150, 14);
+//		credit.setForeground(Color.BLACK);
+//		credit.setVisible(true);
+		
+		JTextField field = new JTextField();
+		field.setBounds(330, 133, 280, 30);
+		field.setForeground(Color.BLUE);
+		field.setVisible(true);
+
+		final JButton button = new JButton("Search");
+		button.setBounds(370, 170, 200, 30);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (button.isEnabled()){
+					Utils.loading();
+					new NameViewer().ViewNames(field);
+				}
 			}
-		}
-		Utils.SetText(Utils.MapToString(map));
+		});
+		button.setVisible(true);
+		
+//		frame.add(credit);
+		frame.add(button);
+		frame.add(field);
 	}
+	
+	protected TextArea createField()
+	  {
+	    TextArea text = new TextArea();
+	    text.setBounds(300, 210, 350, 420);
+	    text.setEditable(false);
+	    text.setBackground(Color.GRAY);
+	    text.setForeground(Color.BLACK);
+	    text.setVisible(true);
+	    
+	    return text;
+	  }
 
 }
